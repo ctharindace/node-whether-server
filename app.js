8581bbf7738a033572f1c21dfbe553f8cbe12274
Chethiya Palliyaguruge <ctharinda@Chethiyas-MacBook-Pro.local>
2018-08-20 23:21:30 +0530

    Changes for heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 
@@ -99,6 +101,6 @@ app.get('/', (requset, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
 });
